abb5f4448a5863e23b64b3523e16d202a6eda4af
barangezen <b.gezenoglu@emakina.com.tr>
2022-10-02 02:24:23 +0300

    Header created and installed packages

---
+++ src/components/BasketItem/BasketItem.tsx
@@ -0,0 +1,41 @@
+import { FormControl, Input, InputAdornment } from "@mui/material";
+import styles from "./BasketItem.module.scss";
+export interface BasketItemProps {
+    productName: string;
+    productPrice: number;
+    productQuantity: number;
+}
+export const BasketItem: React.FC<BasketItemProps> = ({
+    productName,
+    productPrice,
+    productQuantity
+}) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.productDetail}>
+        <div className={styles.name}>{productName}</div>
+        <div className={styles.price}>₺{productPrice}</div>
+      </div>
+      <div className={styles.actions}>
+        <FormControl variant="standard" className={styles.formContainer}>
+          <Input
+            inputProps={{ style: { textAlign: "center" } }}
+            disableUnderline={true}
+            value={productQuantity}
+            className={styles.formInput}
+            startAdornment={
+              <InputAdornment position="start">
+                <span className={styles.adornment}>-</span>
+              </InputAdornment>
+            }
+            endAdornment={
+              <InputAdornment position="end">
+                <span className={styles.adornment}>+</span>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { ShoppingBag } from "@mui/icons-material";
+import { Button, Menu } from "@mui/material";
+import styles from "./Header.module.scss";
+import { BasketMenu } from "../BasketMenu/BasketMenu";
+
+export const Header: React.FC = () => {
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const handleOpenMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseOpenMenu = () => {
+    setAnchorElNav(null);
+  };
+  return (
+    <div className={styles.header}>
+      <Button
+        id="basket-btn"
+        aria-controls="basket"
+        aria-haspopup="true"
+        className={styles.basketIcon}
+        onClick={handleOpenMenu}
+      >
+        <ShoppingBag /> ₺39,97
+      </Button>
+      <Menu
+        anchorEl={anchorElNav}
+        PaperProps={{
+          style: {
+            border: "7px solid #1EA4CE",
+            marginTop: '50px'
+          },
+        }}
+        id="basket-menu"
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+        open={Boolean(anchorElNav)}
+        onClose={handleCloseOpenMenu}
+      >
+        <BasketMenu />
+      </Menu>
+    </div>
+  );
+};
