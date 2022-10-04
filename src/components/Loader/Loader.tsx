99571c79a789327818878a7413db35f736c939ca
barangezen <b.gezenoglu@emakina.com.tr>
2022-10-04 03:32:49 +0300

    Loader added. Block UI used for it

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,19 @@
+import BlockUi from "react-block-ui"
+import "react-block-ui/style.css";
+import { useAppSelector } from "../../store"
+interface ILoader {
+    children: React.ReactNode;
+}
+export const Loader: React.FC<ILoader> = ({
+    children
+}) => {
+    const productsLoader = useAppSelector(state => state.products.loading);
+    const brandsLoader = useAppSelector(state => state.brands.loading);
+    const tagsLoader = useAppSelector(state => state.tags.loading)
+    console.log('loaders',productsLoader, brandsLoader, tagsLoader)
+    return (
+        <BlockUi tag="div" blocking={productsLoader || brandsLoader || tagsLoader} >
+            {children}
+        </BlockUi>
+    )
+}
